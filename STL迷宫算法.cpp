0261ad716f4072f5959bf3fb9603cd69648b7269
defgao <33217367+defgao@users.noreply.github.com>
2018-10-12 14:52:03 +0800

    Create STL迷宫算法.cpp

---
+++ STL迷宫算法.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <ctime>
+#include<stack>
+using namespace std;
+typedef struct point
+{
+    int x;
+    int y;
+}point;
+
+int main() {
+    stack<point> st;
+    int mg2[20][20]={0};//记录该路径是否走过，防止进入死循环
+    int mg[100][100]= {
+            {
+                    0,0,0,0,0,0,0,0,0,0
+            },
+            {
+                    0,1,1,0,1,1,1,0,1,0
+            },
+            {
+                    0,1,1,0,1,1,1,0,1,0
+            },
+            {
+                    0,1,1,1,1,0,0,1,1,0
+            },
+            {
+                    0,1,0,0,0,1,1,1,1,0
+            },
+            {
+                    0,1,1,1,0,1,1,1,1,0
+            },
+            {
+                    0,1,0,1,1,1,0,1,1,0
+            },
+            {
+                    0,1,0,0,0,1,0,0,1,0
+            },
+            {
+                    0,0,1,1,1,1,1,1,1,0
+            },
+            {
+                    0,0,0,0,0,0,0,0,0,0
+            }
+    };
+    int i,j,k;
+    point a,b;
+    a.x=1,a.y=1;
+    st.push(a);
+    while(!(b.x==8&&b.y==8)&&!(st.empty()))
+    {
+        b=st.top();
+        i=b.y;
+        j=b.x;
+        if(mg[i][j+1]==1&&mg2[i][j+1]==0) {
+            b.x=j+1;
+            mg2[i][j+1]=1;
+            st.push(b);
+        }
+        else if(mg[i+1][j]==1&&mg2[i+1][j]==0) {
+            b.y=i+1;
+            mg2[i+1][j]=1;
+            st.push(b);
+        }
+        else if(mg[i][j-1]==1&&mg2[i][j-1]==0) {
+            b.x=j-1;
+            mg2[i][j-1]=1;
+            st.push(b);
+        }
+        else if(mg[i-1][j]==1&&mg2[i-1][j]==0) {
+            b.y=i-1;
+            mg2[i-1][j]=1;
+            st.push(b);
+        }
+        else{ 
+            st.pop();
+        };
+    }
+    while(!(st.empty())){
+        b=st.top();
+        cout<<"x: "<<b.x<<"y: "<<b.y<<endl;
+        st.pop();
+    }
+    return 0;
+}
