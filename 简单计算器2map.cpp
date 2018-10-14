2c1d6ceb0bd560f6f6e5a766c5b2ed54fb14de53
defgao <33217367+defgao@users.noreply.github.com>
2018-10-14 21:15:44 +0800

    Update 简单计算器2map.cpp

--- 简单计算器2map.cpp
+++ 简单计算器2map.cpp
@@ -1,94 +1,117 @@
 #include <iostream>
 #include <stack>
 #include <map>
+#include <string>
 using namespace std;
-map<char , int> inline_stack;
-map<char , int> out_stack;
-void initMap(){
-    inline_stack['#'] = 0;
-    inline_stack['('] = 1;
-    inline_stack['^'] = 7;
-    inline_stack['*'] = 5;
-    inline_stack['/'] = 5;
-    inline_stack['%'] = 5;
-    inline_stack['+'] = 3;
-    inline_stack['-'] = 3;
-    inline_stack[')'] = 8;
 
-    out_stack['#'] = 0;
-    out_stack['('] = 8;
-    out_stack['^'] = 6;
-    out_stack['*'] = 4;
-    out_stack['/'] = 4;
-    out_stack['%'] = 4;
-    out_stack['+'] = 2;
-    out_stack['-'] = 2;
-    out_stack[')'] = 1;
-
-}
-
-
-int optertor(string math_string){
-    stack<char> optr;
-    stack<int> optd;
-    optr.push('#');
-    for(int i = 0 ; i < math_string.length(); i++){
-        if('0' < math_string[i] && math_string[i] < '9'){
-            optd.push(math_string[i] - '0');
-            cout << "这是" << i << "push 进来" << math_string[i] << endl;
-        }else if(math_string[i] == '(' || math_string[i] == '^' || math_string[i] == '*' || math_string[i] == '/' || math_string[i] == '%' || math_string[i] == '+' || math_string[i] == '-' || math_string[i] == ')' || math_string[i] == '#'){
-            if(out_stack[math_string[i]] > inline_stack[optr.top()]){
-                optr.push(math_string[i]);
-                cout << "这是" << i << "push 进来" << math_string[i] << endl;
-            }else if(out_stack[math_string[i]] == inline_stack[optr.top()]) {
-                optr.pop();
-            }else{
-                int temp = optd.top();
-                optd.pop();
-                cout << "此时进行的计算是 " << optd.top()<< optr.top() << temp << endl;
-                switch (optr.top()){
-                    case '+':
-                        optd.top() += temp;
-                        optr.pop();
-                        break;
-                    case '-':
-                        optd.top() -= temp;
-                        optr.pop();
-                        break;
-                    case '*':
-                        optd.top() *= temp;
-                        optr.pop();
-                        break;
-                    case '/':
-                        optd.top() /= temp;
-                        optr.pop();
-                        break;
-                    case '%':
-                        optd.top() %= temp;
-                        optr.pop();
-                        break;
-                    case '^':
-                        for(int i = 0 ; i < temp ; i++){
-                            optd.top() *= optd.top();
-                        }
-                        optr.pop();
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }else{
-            cout << "非法字符" << endl;
-            return -99999;
-        }
-    }
-    return optd.top();
+class Calculator{
+public:
+    string change(string s_mid);
+    int compute(string s_beh);
+    int cal(char a , char b , char opt);
 };
 
+int Calculator::cal(char a, char b, char opt) {
+    int number1 = int(a - '0');
+    int number2 = int(b - '0');
+    switch(opt){
+        case '+':
+            return number1 + number2;
+
+        case '-':
+            return number1 - number2;
+
+        case '*':
+            return number1 * number2;
+
+        case '/':
+            return number1 / number2;
+
+    }
+};
+
+string Calculator::change(string s_mid) {
+    string s_beh="";
+    stack<char> stk;
+    map<char,int> op;
+    op['(']=0;
+    op[')']=0;
+    op['+']=1;
+    op['-']=1;
+    op['*']=2;
+    op['/']=2;
+    string::iterator it=s_mid.begin();;
+    while(it!=s_mid.end())
+    {
+        if(op.count(*it))
+        {
+            if(*it==')')
+            {
+                while(stk.top()!='(')
+                {
+                    s_beh+=stk.top();
+                    stk.pop();
+                }
+                stk.pop();
+            }
+            else if(stk.empty()||*it=='('||op[*it]>op[stk.top()])
+            {
+                stk.push(*it);
+            }
+            else if(op[*it]<=op[stk.top()])
+            {
+                while(op[*it]<=op[stk.top()]&&(!stk.empty()))
+                {
+                    s_beh+=stk.top();
+                    stk.pop();
+                    if(stk.empty()) break;
+                }
+                stk.push(*it);
+            }
+        }
+        else
+        {
+            s_beh+=*it;
+        }
+        it++;
+
+
+        if(it==s_mid.end())//当中缀表达式输出完成，所有元素出栈
+        {
+            while(!stk.empty())
+            {
+                s_beh+=stk.top();
+                stk.pop();
+            }
+            break;
+        }
+    }
+    return s_beh;
+};
+
+int Calculator::compute(string s_beh) {
+  int i = 0;
+  stack <int> result;
+
+  while(s_beh[i] != '\0'){
+      if(s_beh[i] <= '9' && '0' <= s_beh[i]){
+        result.push(s_beh[i]);
+      }else{
+        int temp = result.top();
+        result.pop();
+        int result_number = cal(temp , result.top() , s_beh[i]);
+        cout << "这是第"  << i << "次运算" << result_number << endl;
+        result.pop();
+        result.push(result_number);
+      }
+      i++;
+  }
+  return result.top();
+};
 
 int main(){
-    initMap();
-    cout << optertor("3*(7-2)#");
-
+    Calculator test1;
+    cout << test1.change("1+2*(7+1)") << endl;
+    cout << test1.compute(test1.change("1+2*(7+1)"));
     return 0;
 }
